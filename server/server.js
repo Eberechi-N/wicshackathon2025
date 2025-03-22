49d3693c9b7ebfb7cc707cd4caee2328a5f7fabe
Cleveland Martin IV <tsarycleveland@gmail.com>
2025-03-21 23:44:47 -0500

    comments for server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+//All this cors stuff is to allow us to make requests from the frontend
 const cors = require("cors");
 const corsOptions = {
     origin: ["http://localhost:5173"],
@@ -7,6 +8,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+//This is creating the route "/api"
+//Then i think we are responding to requests to /api with the json
+//holding the array called "fruits"
+//I think maybe here is where you would connect to the database? and send
+//requests between the databse and frontend through this? It may be worth 
+//watching the video to see this part
 app.get("/api", (req, res) => {
     res.json({"fruits": ["apple", "orange", "banana"]});
 });
