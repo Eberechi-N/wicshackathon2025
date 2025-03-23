f9e99a52c307c4275483402e43c399f2531c5498
Eby <133599375+Eberechi-N@users.noreply.github.com>
2025-03-23 11:11:08 -0500

    removed page form main

--- Hackathon2025/src/main.tsx
+++ Hackathon2025/src/main.tsx
@@ -2,14 +2,13 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import Page from "./paget.tsx"
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Theme>
-      <Page />
+      <App />
     </Theme>
   </StrictMode>
 );
