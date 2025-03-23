2cee2a806d326c66b1bafe588ccbd28c380dde99
adambrown00 <arbrown174@yahoo.com>
2025-03-22 21:50:25 -0500

    Please test 3 merge

--- Hackathon2025/src/main.tsx
+++ Hackathon2025/src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import Transactons from "./transactions.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Theme>
       <Transactons />
